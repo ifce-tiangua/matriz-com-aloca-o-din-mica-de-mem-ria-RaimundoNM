30a82c97adbf4f9b0c0ba8e3c7ef265900ac9a9d
RaimundoNM <raimundonm091@gmail.com>
2024-11-18 20:48:40 -0300

    primeiro commit do codigo.c

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
+int main() {
+    int **matriz, taml, tamc, i, j;
+    scanf("%d %d", &taml, &tamc);
+    matriz = (int**)malloc(taml * sizeof(int*));
+    for(i = 0; i < taml; i++) {
+        matriz[i] = (int*)malloc(tamc * sizeof(int));
+    }
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    if(matriz == NULL) {
+        printf("[vazio]");
+    } else {
+        for(i = 0; i < taml; i++) {
+            for (j = 0; j < tamc; j++) {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
 
+        for(i = 0; i < taml; i++) {
+            printf("\n");
+            for (j = 0; j < tamc; j++) {
+                printf("%d ", matriz[i][j]);
+
+            }
+        }
+    }
+
+    for(i = 0; i < taml; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
     return 0;
 }
+
