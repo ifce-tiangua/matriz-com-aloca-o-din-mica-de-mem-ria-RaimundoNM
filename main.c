219bbff6a3ed42b9d426b735f229275994cd4004
RaimundoNM <raimundonm091@gmail.com>
2024-11-18 20:56:52 -0300

    corrigi a saida.c

--- main.c
+++ main.c
@@ -1,37 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
+int main(){
     int **matriz, taml, tamc, i, j;
     scanf("%d %d", &taml, &tamc);
     matriz = (int**)malloc(taml * sizeof(int*));
-    for(i = 0; i < taml; i++) {
+    for(i = 0; i < taml; i++){
         matriz[i] = (int*)malloc(tamc * sizeof(int));
     }
 
-    if(matriz == NULL) {
-        printf("[vazio]");
+    if(matriz == NULL || matriz[i] == 0){
+        printf("[matriz vazia]");
     } else {
-        for(i = 0; i < taml; i++) {
-            for (j = 0; j < tamc; j++) {
+        for(i = 0; i < taml; i++){
+            for (j = 0; j < tamc; j++){
                 scanf("%d", &matriz[i][j]);
             }
         }
 
-        for(i = 0; i < taml; i++) {
+        for(i = 0; i < taml; i++){
             printf("\n");
-            for (j = 0; j < tamc; j++) {
+            for (j = 0; j < tamc; j++){
                 printf("%d ", matriz[i][j]);
 
             }
         }
     }
 
-    for(i = 0; i < taml; i++) {
+    for(i = 0; i < taml; i++){
         free(matriz[i]);
     }
     free(matriz);
     return 0;
-
 }
 
