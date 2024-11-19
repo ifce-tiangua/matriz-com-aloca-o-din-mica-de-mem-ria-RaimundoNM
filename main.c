b17484dd70d509d2159c7063bec953db1108687c
RaimundoNM <raimundonm091@gmail.com>
2024-11-19 18:04:30 -0300

    codigo refeito.c

--- main.c
+++ main.c
@@ -1,36 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
-    int **matriz, taml, tamc, i, j;
-    scanf("%d %d", &taml, &tamc);
+
+int main() {
+    int **matriz, taml, tamc, i, j, k;
+    scanf("%d  %d", &taml, &tamc);
     matriz = (int**)malloc(taml * sizeof(int*));
-    for(i = 0; i < taml; i++){
-        matriz[i] = (int*)malloc(tamc * sizeof(int));
-    }
-
-    if(matriz == NULL || matriz[i] == 0){
+    if(matriz == NULL) {
         printf("[matriz vazia]");
-    } else {
-        for(i = 0; i < taml; i++){
-            for (j = 0; j < tamc; j++){
-                scanf("%d", &matriz[i][j]);
+        
+    }
+
+    for(i = 0; i < taml; i++) {
+        matriz[i] = (int*)malloc(tamc * sizeof(int));
+        if(matriz[i] == NULL) {
+            for(k = 0; k < i; k++) {
+                free(matriz[k]);
             }
+            free(matriz);
+            printf("[matriz vazia]");
+            
         }
 
-        for(i = 0; i < taml; i++){
-            printf("\n");
-            for (j = 0; j < tamc; j++){
-                printf("%d ", matriz[i][j]);
+    }
 
-            }
+    for(i = 0; i < taml; i++) {
+        for(j = 0; j < tamc; j++) {
+            scanf("%d", &matriz[i][j]);
         }
     }
 
-    for(i = 0; i < taml; i++){
+    for(i = 0; i < taml; i++) {
+        for(j = 0; j < tamc; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+
+    }
+    for(i = 0; i < taml; i++) {
         free(matriz[i]);
     }
     free(matriz);
+
     return 0;
 }
 
